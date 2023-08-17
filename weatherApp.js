01f8c0ce33a3749b7e32a31e0304e62787605916
leonbaj <122085077+leonbaj@users.noreply.github.com>
2023-08-17 16:40:19 -0500

    Initializing WeatherApp.js
    Added and initialized some Variables that will be used throughout the code.

---
+++ weatherApp.js
@@ -0,0 +1,14 @@
+const iconElement = document.querySelector(".weather-icon")
+const locationIcon = document.querySelector(".location-icon")
+const tempElement = document.querySelector(".tmeperature-value p")
+const descElement = document.querySelector(".temperature-descritpion p")
+const locationElement = document.querySelector(".location p")
+const notificationElement = document.querySelector(".notification")
+
+var input = document.getElementById("search")
+let city = "";
+let latitude = 0.0;
+let longitude = 0.0;
+
+
+

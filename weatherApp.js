bb38a62df1f932088773caa344428cbb42adc732
leonbaj <122085077+leonbaj@users.noreply.github.com>
2023-08-19 23:46:15 -0500

    Update WeatherApp.js
    Added input/event handler. simply get whats entered and only when the user Presses the Enter key. (need to invoke API response)

--- weatherApp.js
+++ weatherApp.js
@@ -1,3 +1,5 @@
+/* Initializing Variables*/
+
 const iconElement = document.querySelector(".weather-icon")
 const locationIcon = document.querySelector(".location-icon")
 const tempElement = document.querySelector(".tmeperature-value p")
@@ -10,5 +12,18 @@ let city = "";
 let latitude = 0.0;
 let longitude = 0.0;
 
+//Added event Listening looking for when User presses the Enter key.  using keyup (event fired when a key is released) to event the enter key.
+input.addEventListener("keyup", function(event) {
+    //checking if enter is pressed. 
+    if(event.keycode ==13 ){
+        event.preventDefault();
+        
+        //city is assigned value of the input search
+        city=input.value
+        //Display City
+        console.log(city)
+    }
+
+})
 
 
